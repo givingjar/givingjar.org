6f357cde4a17dc1def516698f1e869a97c21a735
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-16 15:15:54 -0400

    Fix lint issues in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
-// Base Gulp File
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    standard = require('gulp-standard'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    cleanCSS = require('gulp-clean-css'),
-    cssBase64 = require('gulp-css-base64'),
-    path = require('path'),
-    notify = require('gulp-notify'),
-    inlinesource = require('gulp-inline-source'),
-    browserSync = require('browser-sync'),
-    imagemin = require('gulp-imagemin'),
-    del = require('del'),
-    cache = require('gulp-cache'),
-    uglify = require('gulp-uglify'),
-    autoprefixer = require('gulp-autoprefixer'),
-    runSequence = require('run-sequence');
+var gulp = require('gulp')
+var standard = require('gulp-standard')
+var sass = require('gulp-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var cleanCSS = require('gulp-clean-css')
+var cssBase64 = require('gulp-css-base64')
+var path = require('path')
+var notify = require('gulp-notify')
+var inlinesource = require('gulp-inline-source')
+var browserSync = require('browser-sync')
+var imagemin = require('gulp-imagemin')
+var del = require('del')
+var cache = require('gulp-cache')
+var uglify = require('gulp-uglify')
+var autoprefixer = require('gulp-autoprefixer')
+var runSequence = require('run-sequence')
 
 // Task to lint JavaScript
 gulp.task('standard', function () {
@@ -38,8 +36,8 @@ gulp.task('sass', function () {
       errLogToConsole: false,
       paths: [ path.join(__dirname, 'scss', 'includes') ]
     })
-    .on("error", notify.onError(function(error) {
-      return "Failed to Compile SCSS: " + error.message;
+    .on('error', notify.onError(function (error) {
+      return 'Failed to Compile SCSS: ' + error.message
     })))
     .pipe(cssBase64())
     .pipe(autoprefixer())
@@ -50,34 +48,34 @@ gulp.task('sass', function () {
     .pipe(browserSync.reload({
       stream: true
     }))
-    .pipe(notify("SCSS Compiled Successfully :)"));
-});
+    .pipe(notify('SCSS Compiled Successfully :)'))
+})
 
 // Task to Minify JS
-gulp.task('jsmin', function() {
+gulp.task('jsmin', function () {
   return gulp.src('./src/js/**/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/'));
-});
+    .pipe(gulp.dest('./dist/js/'))
+})
 
 // Minify Images
-gulp.task('imagemin', function (){
+gulp.task('imagemin', function () {
   return gulp.src('./src/img/**/*.+(png|jpg|jpeg|gif|svg)')
   // Caching images that ran through imagemin
   .pipe(cache(imagemin({
-      interlaced: true
-    })))
-  .pipe(gulp.dest('./dist/img'));
-});
+    interlaced: true
+  })))
+  .pipe(gulp.dest('./dist/img'))
+})
 
 // BrowserSync Task (Live reload)
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync({
     server: {
       baseDir: './src/'
     }
   })
-});
+})
 
 // Gulp Inline Source Task
 // Embed scripts, CSS or images inline (make sure to add an inline attribute to the linked files)
@@ -86,24 +84,24 @@ gulp.task('browserSync', function() {
 gulp.task('inlinesource', function () {
   return gulp.src('./src/**/*.html')
     .pipe(inlinesource())
-    .pipe(gulp.dest('./dist/'));
-});
+    .pipe(gulp.dest('./dist/'))
+})
 
 // Gulp Watch Task
 gulp.task('watch', ['browserSync'], function () {
-   gulp.watch('./src/scss/**/*', ['sass']);
-   gulp.watch('./src/**/*.html').on('change', browserSync.reload);
-});
+  gulp.watch('./src/scss/**/*', ['sass'])
+  gulp.watch('./src/**/*.html').on('change', browserSync.reload)
+})
 
 // Gulp Clean Up Task
-gulp.task('clean', function() {
-  del('dist');
-});
+gulp.task('clean', function () {
+  del('dist')
+})
 
 // Gulp Default Task
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'])
 
 // Gulp Build Task
-gulp.task('build', function() {
-  runSequence('clean', 'standard', 'sass', 'imagemin', 'jsmin', 'inlinesource');
-});
+gulp.task('build', function () {
+  runSequence('clean', 'standard', 'sass', 'imagemin', 'jsmin', 'inlinesource')
+})
