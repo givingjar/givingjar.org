62786c8e6bc2ff939f693c8d246d2b6cc56668df
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-17 15:27:31 -0400

    Use lowercase component files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+import App from '@/app'
+import router from '@/router'
 
 /* eslint-disable no-new */
 new Vue({
