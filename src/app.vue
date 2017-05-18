6c2785c49c7fd8d70a42705196258bd4d9e05b8c
Erik Gillespie <erik.gillespie@gmail.com>
2017-05-17 21:01:50 -0400

    Introducing Skeleton for basic grid and style consistency

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="static/img/giving-jar-icon-square.svg">
     <router-view></router-view>
   </div>
 </template>
@@ -14,11 +14,24 @@ export default {
 <style lang="stylus">
 @import './design'
 
-#app
+*
   font-family: $design.base.font.family
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
-  text-align: center
+  background-color: $design.base.background.color
   color: $design.base.color
+
+h1, h2, h3, h4, h5, h6
+  font-family: $design.heading.font.family
+
+a
+  color: $design.brand.colors.action
+  text-decoration: none
+  &:hover
+    color: darken($design.brand.colors.action, 15%)
+    text-decoration: underline
+
+#app
+  text-align: center
   margin-top: 60px
 </style>
