c9ce8117bfb820ece8fbe40d585d161e548c6937
Erik Gillespie <erik.gillespie@gmail.com>
2017-07-20 15:08:04 -0400

    Introduce keyframe animations, only show starting slide

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,7 +1,11 @@
 <template>
   <section id="carousel">
     <div class="all-slides">
-      <div class="slide" v-for="(slots, slotName) in $slots">
+      <div
+        v-for="(slots, slotName, index) in $slots"
+        v-show="index === activeSlide"
+        class="slide fadeInFromRight"
+      >
         <slot :name="slotName"/>
       </div>
     </div>
@@ -12,7 +16,7 @@
 export default {
   data () {
     return {
-      slidesMax: 3
+      activeSlide: 0
     }
   }
 }
@@ -22,4 +26,60 @@ export default {
 #carousel
   margin-bottom: 4rem
   width: 100%
+
+.slide
+  animation-duration: 1s
+  animation-fill-mode: both
+  display: inline-block
+  width: 100%
+
+//
+// CAROUSEL ANIMATIONS
+//
+
+@keyframes fadeInFromLeft
+  from
+    opacity: 0
+    transform: translate(-100%, 0)
+
+  to
+    opacity: 1
+    transform: none
+
+.fadeInFromLeft
+  animation-name: fadeInFromLeft
+
+@keyframes fadeInFromRight
+  from
+    opacity: 0
+    transform: translate(100%, 0)
+
+  to
+    opacity: 1
+    transform: none
+
+.fadeInFromRight
+  animation-name: fadeInFromRight
+
+@keyframes fadeOutToLeft
+  from
+    opacity: 1
+
+  to
+    opacity: 0
+    transform: translate(-100%, 0)
+
+.fadeOutToLeft
+  animation-name: fadeOutToLeft
+
+@keyframes fadeOutToRight
+  from
+    opacity: 1
+
+  to
+    opacity: 0
+    transform: translate(100%, 0)
+
+.fadeOutToRight
+  animation-name: fadeOutToRight
 </style>
--- src/layouts/landing.vue
+++ src/layouts/landing.vue
@@ -1,12 +1,26 @@
 <template>
   <Layout>
     <Carousel>
-      <BlogSlide
+      <FeatureImageSlide
         slot="0"
-        imageUrl="https://blog.givingjar.org/img/spotlight/michigan-womens-historical-center/michigan-womens-historical-center.jpg"
-        title="Charity Spotlight: Michigan Women's Historical Center and Hall of Fame"
-        subtitle="Supporting the equality of women by sharing their achievements in Michigan's history."
-        linkUrl="https://blog.givingjar.org"
+        imageUrl="https://blog.givingjar.org/img/spotlight/men-making-a-difference/men-making-a-difference-cookout.jpg"
+        title="Charity Spotlight: Men Making a Difference"
+        subtitle="Demonstrating the importance of family and volunteerism in our community."
+        linkUrl="https://blog.givingjar.org/charity-spotlight-men-making-a-difference/"
+      />
+      <FeatureImageSlide
+        slot="1"
+        imageUrl="https://blog.givingjar.org/img/spotlight/leonies-longing/leonies-longing-logo.jpg"
+        title="Charity Spotlight: Leonie's Longing"
+        subtitle="A community of support for women who have returned to lay life from one of religion."
+        linkUrl="https://blog.givingjar.org/charity-spotlight-leonies-longing/"
+      />
+      <FeatureImageSlide
+        slot="2"
+        imageUrl="https://blog.givingjar.org/img/paypal-giving-fund/ppgf-do-more-good.jpg"
+        title="How To: Enroll in PayPal Giving Fund"
+        subtitle="Get 100% of every PayPal donation and open up new ways to raise funds online."
+        linkUrl="https://blog.givingjar.org/how-to-enroll-in-paypal-giving-fund/"
       />
     </Carousel>
     <slot></slot>
@@ -14,13 +28,13 @@
 </template>
 
 <script>
-import BlogSlide from '@/components/carousel-blog-slide'
+import FeatureImageSlide from '@/components/carousel-feature-image-slide'
 import Carousel from '@/components/carousel'
 import Layout from '@/layouts/main'
 
 export default {
   components: {
-    BlogSlide, Carousel, Layout
+    FeatureImageSlide, Carousel, Layout
   }
 }
 </script>
