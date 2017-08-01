4cd813403b498852bd3bb99c765ff0d45e443a0e
Erik Gillespie <erik.gillespie@gmail.com>
2017-08-01 15:52:04 -0400

    Basic tests for carousel

---
+++ test/unit/specs/components/carousel.spec.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Carousel from '@/components/carousel'
+
+const newCarousel = () => {
+  return new Vue({
+    template: [
+      '<Carousel>',
+      '<h1 slot="0">Slot 0</h1>',
+      '<h1 slot="1">Slot 1</h1>',
+      '<h1 slot="2">Slot 2</h1>',
+      '</Carousel>'
+    ].join(''),
+    components: { Carousel }
+  }).$mount()
+}
+
+describe('carousel.vue', () => {
+  it('contains one slide per slot', () => {
+    const vm = newCarousel()
+    expect(vm.$el.getElementsByClassName('slide').length)
+      .to.equal(3)
+  })
+
+  it('contains one navigation picker per slot', () => {
+    const vm = newCarousel()
+    expect(vm.$el.getElementsByClassName('picker').length)
+      .to.equal(3)
+  })
+
+  it('contains previous and next navigation links', () => {
+    const vm = newCarousel()
+    const navLinks = vm.$el.querySelectorAll('#carousel nav a')
+    expect(navLinks.length).to.be.at.least(2)
+    expect(navLinks[0].title).to.contain('previous')
+    expect(navLinks[navLinks.length - 1].title).to.contain('next')
+  })
+})
