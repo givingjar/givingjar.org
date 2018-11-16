274ed2396c1f1388cb1d693cc59311917051b351
Erik Gillespie <erik.gillespie@gmail.com>
2018-11-16 09:09:45 -0500

    Move JavaScript to separate file

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || []
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date())
+gtag('config', 'UA-62666024-4')
+
+// Smooth auto-scrolling
+$.localScroll()
+
+// Newsletter signup form
+$('#mc-embedded-subscribe-form').ajaxChimp({
+  callback: function(resp) {
+    if (resp.result === 'success') {
+      $('#mc-embedded-subscribe-form input').hide()
+      $('#mce-email-label').html('You&apos;ll receive a confirmation email soon. Thank you for your interest in Giving Jar! <i class="fa fa-heart"></i>')
+    }
+    $('#mc-embedded-subscribe-form a').attr('target', '_blank')
+  }
+})
+
+// Video takeover popup
+var takeover = document.querySelector('.takeover-background')
+var takeoverCloseIcon = document.querySelector('.takeover-close-icon')
+
+function showTakeover () {
+  document.body.classList.toggle('noscroll', true)
+  takeover.setAttribute('aria-hidden', false)
+  takeover.scrollTop = 0
+}
+
+function hideTakeover () {
+  document.body.classList.toggle('noscroll', false)
+  takeover.setAttribute('aria-hidden', true)
+  window.scrollTo(0, 0)
+  document.getElementById('mce-EMAIL').focus()
+}
+
+takeoverCloseIcon.addEventListener('click', hideTakeover)
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'Escape' || event.which === 27) {
+    hideTakeover()
+  }
+})
+
+showTakeover()
