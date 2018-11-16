6d8243c21cb55b07e310afc73f7f33398a7adf4a
Erik Gillespie <erik.gillespie@gmail.com>
2018-11-16 10:07:08 -0500

    Add links to show takeover

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ var takeover = document.querySelector('.takeover-background')
 var takeoverCloseIcon = document.querySelector('.takeover-close-icon')
 
 function showTakeover () {
+  document.getElementById('show-drop-menu').checked = false
   document.body.classList.toggle('noscroll', true)
   takeover.setAttribute('aria-hidden', false)
   takeover.scrollTop = 0
@@ -31,8 +32,8 @@ function showTakeover () {
 function hideTakeover () {
   document.body.classList.toggle('noscroll', false)
   takeover.setAttribute('aria-hidden', true)
-  window.scrollTo(0, 0)
   document.getElementById('mce-EMAIL').focus()
+  window.scrollTo(0, 0)
 }
 
 takeoverCloseIcon.addEventListener('click', hideTakeover)
@@ -41,11 +42,17 @@ document.addEventListener('keyup', function (event) {
     hideTakeover()
   }
 })
+document.querySelectorAll('.takeover-link').forEach(function (link) {
+  link.addEventListener('click', showTakeover)
+})
 
 var startDate = new Date('2018-11-20T00:00:00-0500').getTime()
 var endDate = new Date('2018-12-31T23:59:59-0500').getTime()
 var now = Date.now()
 
 if (now >= startDate && now <= endDate) {
+  document.querySelectorAll('.takeover-link').forEach(function (link) {
+    link.classList.toggle('takeover-link', false)
+  })
   showTakeover()
 }
