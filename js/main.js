a0b5829a7b715caff2f6b6e15046504803af50b4
Erik Gillespie <erik.gillespie@gmail.com>
2019-11-06 12:00:56 -0500

    Remove takeover popup

--- js/main.js
+++ js/main.js
@@ -17,36 +17,3 @@ $('#mc-embedded-subscribe-form').ajaxChimp({
     $('#mc-embedded-subscribe-form a').attr('target', '_blank')
   }
 })
-
-// Video takeover popup
-var takeover = document.querySelector('.takeover-background')
-var takeoverCloseIcon = document.querySelector('.takeover-close-icon')
-
-function showTakeover () {
-  document.getElementById('show-drop-menu').checked = false
-  document.body.classList.toggle('noscroll', true)
-  takeover.setAttribute('aria-hidden', false)
-  takeover.scrollTop = 0
-}
-
-function hideTakeover () {
-  document.body.classList.toggle('noscroll', false)
-  takeover.setAttribute('aria-hidden', true)
-  document.getElementById('mce-EMAIL').focus()
-  window.scrollTo(0, 0)
-}
-
-takeoverCloseIcon.addEventListener('click', hideTakeover)
-document.addEventListener('keyup', function (event) {
-  if (event.key === 'Escape' || event.which === 27) {
-    hideTakeover()
-  }
-})
-document.querySelectorAll('.takeover-link').forEach(function (link) {
-  link.addEventListener('click', showTakeover)
-})
-
-document.querySelectorAll('.takeover-link').forEach(function (link) {
-  link.classList.toggle('takeover-link', false)
-})
-showTakeover()
